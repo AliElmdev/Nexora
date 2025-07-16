f50175a339d56bfae002e5c240060b1062582fe4
AliElmdev <alielmakhroubi00@gmail.com>
2025-07-16 21:21:31 +0200

    fix broken pipe

---
+++ lib/services/realtimeService.ts
@@ -0,0 +1,6 @@
+// Placeholder for realtimeService
+export const realtimeService = {
+  subscribeToRoom: (roomId: string, callback: (message: any) => void) => {},
+  unsubscribeFromRoom: (roomId: string) => {},
+  setLastMessageId: (roomKey: string, messageId: string) => {},
+}; 
